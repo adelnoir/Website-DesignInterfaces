9f2b9078e983372fce6d8c45d2b0af67b3842822
Leonardo Peixoto <leonardopeixoto2005@gmail.com>
2026-01-20 15:04:29 +0000

    Segundo commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+function abrirConteudo(numeroPagina)
+{
+    document.getElementById("gridPaginas").style.display = 'none';
+    document.getElementById("pagina" + numeroPagina).style.display = 'block';
+}
+
+function fecharConteudo()
+{
+    document.getElementById("gridPaginas").style.display = 'grid';
+    // Ocultar todas as páginas de conteúdo
+    for (let i = 1; i <= 6; i++) {
+        document.getElementById("pagina" + i).style.display = 'none';
+    }
+}
+
+// Inicializar ao carregar a página
+window.addEventListener('DOMContentLoaded', inicializarModoEscuro);
